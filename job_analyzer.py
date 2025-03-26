3639bab919dba9fe436f078030d7ace05c6b3ec5
Aaro Korpela <korpelanaaro@gmail.com>
2025-03-26 23:17:16 +0200

    practicing functions, classes, modules, libraries with a little bit of data

---
+++ job_analyzer.py
@@ -0,0 +1,7 @@
+def calculate_salary(base_salary,bonus_rate=.1):
+  """
+  Calc the total salary based on base and bonus --> base*(1+bonus)
+  """
+  return base_salary*(1+bonus_rate)
+def calculate_bonus(base_salary,bonus_rate=.1):
+  return base_salary*bonus_rate
